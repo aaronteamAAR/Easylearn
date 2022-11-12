ffa5a413128a74594bbdad82c3608ff1da529563
Aaron Chris <rooneychris83@gmail.com>
2022-11-12 21:55:15 +0100

    bugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,7 @@ devOptions: {
     output: 'server',
     adapter: vercel(),
     // ...
-    output: "server",
-    adapter: node({
-      mode: "middleware"
-    }),
+  
   
     integrations: [tailwind(), react()],
   });
