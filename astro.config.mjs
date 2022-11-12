f2fc8604714ef6b4c249fd3351524d3fbf3e0c27
Aaron Chris <rooneychris83@gmail.com>
2022-11-12 21:31:31 +0100

    Added vercel json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 devOptions: {
     tailwindConfig: './tailwind.config.js';
   }
 
   export default defineConfig({
+    output: 'server',
+    adapter: vercel(),
     // ...
     output: "server",
     adapter: node({
