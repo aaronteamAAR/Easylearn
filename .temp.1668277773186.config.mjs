d993e7860dfb6b0b1a8ac77c2705e14344081e35
Aaron Chris <rooneychris83@gmail.com>
2022-11-12 20:20:08 +0100

    node js added

--- .temp.1668277773186.config.mjs
+++ .temp.1668277773186.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import react from '@astrojs/node';
 
 devOptions: {
     tailwindConfig: './tailwind.config.js';
@@ -9,5 +9,11 @@ devOptions: {
 
   export default defineConfig({
     // ...
+
+   output: 'server',
+   adapter: node({
+    mode: 'middleware'
+   }),
+
     integrations: [tailwind(), react()],
   });
